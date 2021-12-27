0f00558493b4b05612411c1646b67d5d6fa02958
hari <39596845+hariuserx@users.noreply.github.com>
2021-12-27 00:57:02 -0800

    Create monitor.sh

---
+++ monitor.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+LOG_FILE="$1"
+
+process_logs() {
+      read -r LINE
+      while true
+      do
+          HTTP_PATH=$(echo "$LINE" | awk '($9 == "500" && $8 ~ /^HTTP/) {print $7}') # Check for HTTP 500 errors
+          if [ -n "$HTTP_PATH" ]
+          then
+            echo "error found while calling $HTTP_PATH"  # Do Something if error is found
+          fi
+
+          read -r LINE
+      done
+}
+
+
+tail -n0 -f "$LOG_FILE" | process_logs
